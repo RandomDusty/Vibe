d0eda766243127ff889214b7da42bdf0826d772c
RandomDusty <111452230+RandomDusty@users.noreply.github.com>
2022-08-20 01:02:31 +0300

    trying to fix deploy css problem

--- client/layouts/MainLayout.tsx
+++ client/layouts/MainLayout.tsx
@@ -1,63 +1,72 @@
 import React, {memo, PropsWithChildren} from 'react';
 import Navbar from '../components/Navbar'
 import {Card, Container} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Player from "../components/Player";
 import Head from "next/head";
-import {styled} from "@mui/material/styles";
 
 interface MainLayoutProps {
     title?: string;
 }
 
+const theme = createTheme({
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: {
+                body: {
+                    backgroundColor: "#141414",
+                    color: "#fffcff",
+                }
+            }
+        }
+    }
+});
+
 const MainLayout: React.FC<PropsWithChildren<MainLayoutProps>> = memo(({children, title}) => {
     return (
-        <React.Fragment>
-            <style jsx global>{`
-              body {
-                background: #141414;
-                color: #fffcff;
-              }
-            
-              ::-webkit-scrollbar {
-                height: 9px;
-                width: 9px;
-                background-color: #2e2e2e;
-              }
-            
-              ::-webkit-scrollbar-thumb {
-                border-radius: 10px;
-                background-color: #fffcff;
-              }
-            
-              ::-webkit-scrollbar-track {
-                -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.2);
-                border-radius: 10px;
-                background-color: #2e2e2e;
-              }
-            
-            
-            
-            .player {
-              display: flex;
-              flex-direction: column;
-              justify-content: center;
-              align-items: center;
-            
-            }
-            `}</style>
+        <ThemeProvider theme={theme}>
+            <React.Fragment>
+                <style jsx global>{`
+                
+                  ::-webkit-scrollbar {
+                    height: 9px;
+                    width: 9px;
+                    background-color: #2e2e2e;
+                  }
+                
+                  ::-webkit-scrollbar-thumb {
+                    border-radius: 10px;
+                    background-color: #fffcff;
+                  }
+                
+                  ::-webkit-scrollbar-track {
+                    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.2);
+                    border-radius: 10px;
+                    background-color: #2e2e2e;
+                  }           
+                  
+                .player {
+                  display: flex;
+                  flex-direction: column;
+                  justify-content: center;
+                  align-items: center;
+                
+                }
+                `}</style>
 
-            <Head>
-                <title>{title || 'Vibe'}</title>
-            </Head>
-            <Navbar/>
-                <Container style={{marginTop: '100px'}}>
-                    {children}
-                </Container>
-            <div className="player">
-                <Player/>
-            </div>
+                <Head>
+                    <title>{title || 'Vibe'}</title>
+                </Head>
+                <Navbar/>
+                    <Container style={{marginTop: '100px'}}>
+                        {children}
+                    </Container>
+                <div className="player">
+                    <Player/>
+                </div>
 
-        </React.Fragment>
+            </React.Fragment>
+        </ThemeProvider>
     );
 });
 
