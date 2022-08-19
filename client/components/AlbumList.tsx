b603f4bdac41a8923112c410a7d99df671aee27c
RandomDusty <111452230+RandomDusty@users.noreply.github.com>
2022-08-19 23:16:20 +0300

    trying to fix deploy css problem

--- client/components/AlbumList.tsx
+++ client/components/AlbumList.tsx
@@ -2,12 +2,12 @@ import React, {memo} from 'react';
 import Loader from "./Loader";
 import {Box, Card, CardContent, Grid} from "@mui/material";
 import {IAlbum} from "../types/album";
-import AlbumItem from "./AlbumItem";
 import mainPageStyles from '../styles/MainPageAlbumList.module.scss'
 import styles from '../styles/Album.module.scss'
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import {useRouter} from "next/router";
+const AlbumItem = React.lazy( () => import("./AlbumItem"));
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 interface AlbumListProps {
@@ -27,21 +27,20 @@ const AlbumList: React.FC<AlbumListProps> = memo(({albums, page}) => {
                                 album={album}
                                 page={page ? page : ''}
                             />
-                        })
-                    // )}
-                    // {page == 'main'
-                    //     ? <Grid item xs={8} sx={{margin: 'auto'}}>
-                    //         <Card className={mainPageStyles.allAlbumButton} onClick={() => router.push('/albums/', undefined, { shallow: true })}>
-                    //             <CardContent >
-                    //                 <Typography  variant='overline'>
-                    //                     <b style={{textAlign: 'center'}}>Все альбомы </b>
-                    //                 </Typography>
-                    //             </CardContent>
-                    //         </Card>
-                    //     </Grid>
-                    //     : <div></div>
-                    // }
+                    })
                 }
+                    {page == 'main'
+                        ? <Grid item xs={8} sx={{margin: 'auto'}}>
+                            <Card className={mainPageStyles.allAlbumButton} onClick={() => router.push('/albums/', undefined, { shallow: true })}>
+                                <CardContent >
+                                    <Typography  variant='overline'>
+                                        <b style={{textAlign: 'center'}}>Все альбомы </b>
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        : <div></div>
+                    }
             </Grid>
         </React.Suspense>
     );
