0b5e895722ede25eb6ca4117cf8372e46f827675
RandomDusty <111452230+RandomDusty@users.noreply.github.com>
2022-08-19 21:07:41 +0300

    Demo version of the site with minimal functionality

--- client/components/AlbumItem.tsx
+++ client/components/AlbumItem.tsx
@@ -18,7 +18,6 @@ const AlbumItem: React.FC<AlbumItemProps> = memo(({album, page}) => {
     const router = useRouter();
 
     return (
-        <Grid item xs={page=='main'?8:4} className={styles.gridItem}>
             <Card className={page=='main'?mainPageStyles.albumItem:styles.albumItem} onClick={() => router.push('/albums/' + album._id, undefined, { shallow: true })}>
                 <CardMedia
                     component="img"
@@ -35,7 +34,6 @@ const AlbumItem: React.FC<AlbumItemProps> = memo(({album, page}) => {
                     </Typography>
                 </CardContent>
             </Card>
-        </Grid>
     );
 });
 
